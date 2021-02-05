171a916e3a0ff1a959a922397a32bda167e2b87f
Bryce Belchehr <74574502+Bryce259@users.noreply.github.com>
2021-02-05 14:45:05 -0500

    Two newe files.
    These new batch files can tell jokes and a user account flooder. The file Jokes.bat will give you jokes.

---
+++ Jokes.bat
@@ -0,0 +1,42 @@
+@echo off
+echo hi and welcome to jokes, ware you get all the good jokes. press inni key to begin
+pause
+echo what's red and bad for your teath? A brick.
+pause
+echo Remember the best angle to to approach A problem from is the "try "angle.
+pause
+echo To be Frank, I'd have to change my name.
+echo Why didn't the pirate see the new pirat movie? because it was rated R.
+pause
+echo What biscuit does a short person like? Shortbred
+pause
+echo why don't sharks eat clowns? because they taste funny!
+pause
+echo What's the difference between a hippo and a zippo? one is really heavy, the other is a little lighter.
+pause
+echo I 8 a clock. yesterday. It was so time consuming.
+pause
+echo Mountains aren't just funny, they are hill areas!
+pause
+echo Two rabbits are in a garden and one of the rabbits says, "Thith carrot tathes pithy."  The other rabbit says, "Yes, I know, I just pithed on it."
+pause
+echo There’s a sweet old couple happily living life. One day the wife went in for a medical exam and when she came home she reported to her husband, "The doctor says I have the heart of a 50-year-old, lungs of a 40-year-old, and the blood pressure of a 25-year-old." The huband replies, "Oh really? And what did he say about your 70-year-old ass?" She replied, "He never mentioned you."
+pause
+echo A 2nd grade teacher decides to teach sex ed. to her class. She starts out by drawing a penis on the chalk board and asks the class, "Does anyone know what this is?" And little Johnny says, "Yes, my dad has 2 of them!" And the teacher says, "Are you sure about that?" And little Johnny says, "Yes, he uses a small skinny one to go to the bathroom, and a big long one to brush the babysitter's teeth."
+pause
+echo A grasshopper goes into a bar. The bartender notices him and says, "Hey! We've got a drink named after you." The grasshopper says, "You've got a drink named James?" Never raise your hand to your children; it leaves your midsection unprotected. -- Robert Orben
+pause
+echo Two hunters are out in the woods and one of them is taking a piss on a tree when a snake bites him on the penis. By the time they get to a phone the bite victim is near death. His friend calls a doctor in the nearby town and the doctor says, "He'll die before I get there unless you get the poison out. You need to make a small incision and suck the poison out." He says, "OK doc," and he hangs up. The near-death hunter asks, "What did he say?" "You're gonna die."
+pause
+echo Justice Brewer was asked by a man, "Will you please tell me, sir, what is the penalty for bigamy? "Justice Brewer smiled and answered, "Two mothers-in-law."
+pause
+Dear Tech Support, Last year I upgraded from Boyfriend 5.0 to Husband 1.0 and noticed a distinct slow down in overall system performance -- particularly in the flower and jewelry applications, which operated flawlessly under Boyfriend 5.0. In addition, Husband 1.0 uninstalled many other valuable programs, such as Romance 9.5 and Personal Attention 6.5 and then installed undesirable programs such as NFL 5.0, NBA 3.0, and Golf Clubs 4.1. Conversation 8.0 no longer runs, and Housecleaning 2.6 simply crashes the system. I've tried running Nagging 5.3 to fix these problems, but to no avail.
+What can I do? Signed, Desperate
+pause
+echo Two antennas met on a roof, fell in love and got married. The ceremony wasn’t much, but the reception was excellent.
+pause
+A beautiful young New York woman was so depressed that she decided to end her life by throwing herself into the ocean. But just before she could throw herself from the docks, a handsome young sailor stopped her. "You have so much to live for," said the sailor. "Look, I’m off to Europe tomorrow and I can stow you away on my ship. I’ll take care of you, bring you food every day, and keep you happy." With nothing to lose, the woman accepted. That night the sailor brought her aboard and hid her in a lifeboat. From then on, every night he would bring her three sandwiches and make love to her until dawn. Three weeks later she was discovered by the captain during a routine inspection. "What are you doing here?" asked the captain. "I have an arrangement with one of the sailors," she replied. "He brings me food and I get a free trip to Europe. Plus he’s screwing me." "He certainly is," replied the captain. "This is the Staten Island Ferry."
+pause
+echo Why did the yoga instructer by air freshener? The feat indexx was rising
+pause
+echo What kind of salad wares a dyper? Baby greens!
